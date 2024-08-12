13432ab3dd51ac850f7dfaafe3ec3d91d8b7efb7
mage <secret.superdev568@gmail.com>
2024-08-12 16:24:12 -0500

    fix serviceImpl

--- src/main/java/com/springboot/record_system/service/UserServiceImpl.java
+++ src/main/java/com/springboot/record_system/service/UserServiceImpl.java
@@ -1,10 +1,8 @@
 package com.springboot.record_system.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import com.springboot.record_system.dto.UserDTO;
 import com.springboot.record_system.model.User;
 import com.springboot.record_system.repository.UserRepository;
 
@@ -15,18 +13,22 @@ public class UserServiceImpl implements UserService {
   @Autowired
   private UserRepository userRepository;
 
+  @Override
   public List<User> getAllUser() {
     return userRepository.findAll();
   }
 
+  @Override
   public User createUser(User user) {
     return userRepository.save(user);
   }
 
+  @Override
   public User updateUser(User user) {
     return userRepository.save(user);
   }
 
+  @Override
   public void deleteUserById(String id) {
     userRepository.deleteById(id);
   }
